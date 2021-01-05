e9989a30b0d48023cb1a72a2d2cda81932bf7285
divyangju1991 <51605929+divyangju1991@users.noreply.github.com>
2021-01-05 10:28:22 +0530

    Iterative solution

--- Arrays/Sum_of_Array_Elements.java
+++ Arrays/Sum_of_Array_Elements.java
@@ -1,1 +1,14 @@
-
+class Sum_of_Array_Elements
+{
+    public static int sumElement(int arr[], int n)
+    {
+        // Your code here
+        int sum = 0;
+        
+        for(int i=0; i<n; i++){
+            sum += arr[i];
+        }
+        
+        return sum;
+    }
+}
