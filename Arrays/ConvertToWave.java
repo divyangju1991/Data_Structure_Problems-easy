fbd298ebb3d6e845f87ca234e11a9d6022cfc143
divyangju1991 <51605929+divyangju1991@users.noreply.github.com>
2021-01-05 10:53:17 +0530

    Iterative solution and last element handle
    Iterative solution and last element handle if it is even

---
+++ Arrays/ConvertToWave.java
@@ -0,0 +1,29 @@
+/*
+Input:
+N = 5
+arr[] = {1,2,3,4,5}
+Output: 2 1 4 3 5
+Explanation: Array elements after 
+sorting it in wave form are 
+2 1 4 3 5.
+*/
+class convertToWave
+{
+
+	public void convertToWave(int arr[], int n){
+    
+		// Your code here
+		
+		for(int i=1; i<n-1; i = i+2){
+			int tmp = arr[i-1];
+			arr[i-1] = arr[i];
+			arr[i] = tmp;
+		}
+		
+		if(n%2 == 0){
+			int tmp = arr[n-2];
+			arr[n-2] = arr[n-1];
+			arr[n-1] = tmp;
+		}
+	}
+}
